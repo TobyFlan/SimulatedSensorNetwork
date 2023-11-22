f82a6996c80e4531dd65e362f1b244608cd075c8
Toby Flanagan <sc21tf@leeds.ac.uk>
2023-11-22 18:25:53 +0000

    task 3 complete

--- process_data/index.js
+++ process_data/index.js
@@ -1,15 +1,46 @@
 
 module.exports = async function (context, sqlChanges, sqlData, processData) {
 
-    //array for total sensor values with 20 values
+    //array to buffer the output to database
+    const sensorMetaDataArray = [];
+    
+    //loop through all 20 data entries
+    for(i = 0; i < sqlData.length; i++){
+  
+        const change = sqlData[i];
+
+        sensorMetaDataArray.push({
+
+            //update values for sensors 1 to 20
+            Id: change.SensorID,
+
+            //update temperature values
+            TempMax: change.MaxTemperature,
+            TempMin: change.MinTemperature,
+            TempAvg: change.AvgTemperature,
+
+            //update wind speed values
+            WindMax: change.MaxWind,
+            WindMin: change.MinWind,
+            WindAvg: change.AvgWind,
+
+            //update humidity values
+            HumidMax: change.MaxHumidity,
+            HumidMin: change.MinHumidity,
+            HumidAvg: change.AvgHumidity,
+
+            //update co2 values
+            CO2Max: change.MaxCO2,
+            CO2Min: change.MinCO2,
+            CO2Avg: change.AvgCO2
+
+        });   
+
+    }
 
 
-    console.log(JSON.stringify(sqlData));
-
-
-
-
-    //so this function finds the averatges of the newly added 100 table entries. Need to find average with old ones.
+    //push this data to database
+    context.bindings.processData = JSON.stringify(sensorMetaDataArray);
 
 
 };
